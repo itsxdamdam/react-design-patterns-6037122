5b309e4bd2e311db85601f6f9dabb302d19e9155
Shaun Wassell <shaun.p.wassell@gmail.com>
2025-03-11 20:05:15 +0000

    Added an uncontrolled form

--- controlled-uncontrolled/src/App.jsx
+++ controlled-uncontrolled/src/App.jsx
@@ -1,22 +1,37 @@
-import { useState } from 'react';
-import { ControlledForm } from "./ControlledForm"
-import { ControlledModal } from "./ControlledModal"
-import { UncontrolledForm } from "./UncontrolledForm"
+import { UncontrolledOnboardingFlow } from './UncontrolledOnboardingFlow';
+
+const StepOne = ({ goToNext }) => (
+	<>
+	<h1>Step 1</h1>
+	<button onClick={() => goToNext({ name: 'John Doe' })}>Next</button>
+	</>
+);
+
+const StepTwo = ({ goToNext }) => (
+	<>
+	<h1>Step 2</h1>
+	<button onClick={() => goToNext({ age: 100 })}>Next</button>
+	</>
+);
+
+const StepThree = ({ goToNext }) => (
+	<>
+	<h1>Step 3</h1>
+	<button onClick={() => goToNext({ hairColor: 'brown' })}>Next</button>
+	</>
+);
 
 function App() {
-  const [showModal, setShowModal] = useState(false);
-
-  return (
-    <>
-      <ControlledModal shouldShow={showModal} onRequestClose={() => {}}>
-        <ControlledForm
-          initialName="Shaun"
-          initialAge={123}
-          initialHairColor="Brown" />
-      </ControlledModal>
-      <button onClick={() => setShowModal(true)}>Open Modal</button>
-    </>
-  )
+	return (
+		<UncontrolledOnboardingFlow onFinish={data => {
+			console.log(data);
+			alert('Onboarding complete!');
+		}}>
+			<StepOne />
+			<StepTwo />
+			<StepThree />
+		</UncontrolledOnboardingFlow>
+	);
 }
 
-export default App
+export default App;
