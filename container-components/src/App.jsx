051a526547ac48f048ef39375fa6f7b41f4946bc
Shaun Wassell <shaun.p.wassell@gmail.com>
2025-03-11 17:45:59 +0000

    Added a resource loader component

--- container-components/src/App.jsx
+++ container-components/src/App.jsx
@@ -1,17 +1,18 @@
 import { CurrentUserLoader } from "./CurrentUserLoader"
+import { ResourceLoader } from "./ResourceLoader"
 import { UserInfo } from "./UserInfo"
 import { UserLoader } from "./UserLoader"
 
 function App() {
   return (
     <>
+    <ResourceLoader
+      resourceUrl={`/api/users/345`}
+      resourceName="user"
+    >
+      <UserInfo />
+    </ResourceLoader>
     <UserLoader userId="345">
-      <UserInfo />
-    </UserLoader>
-    <UserLoader userId="234">
-      <UserInfo />
-    </UserLoader>
-    <UserLoader userId="123">
       <UserInfo />
     </UserLoader>
     </>
