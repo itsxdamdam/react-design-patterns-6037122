8fda5169c2a210039be051f46d5d5c3be069ff6e
Shaun Wassell <shaun.p.wassell@gmail.com>
2025-03-12 18:35:19 +0000

    Partial application example :

--- functional-react/src/partialApplication.jsx
+++ functional-react/src/partialApplication.jsx
@@ -7,3 +7,17 @@ export const Button = ({ size, color, text, ...props }) => {
 		}} {...props}>{text}</button>
 	);
 }
+
+function getPropertyWithDefault(key, defaultValue, obj) {
+  if (key in obj) {
+    return obj[key];
+  }
+
+  return defaultValue;
+}
+
+function getName(obj) {
+  return getPropertyWithDefault('firstName', 'N/A', obj);
+}
+
+getName({ name: 'Shaun' })
