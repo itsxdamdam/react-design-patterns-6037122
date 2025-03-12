30dc7c73b71a219ab134602020eb1269d7337b84
Shaun Wassell <shaun.p.wassell@gmail.com>
2025-03-12 18:29:03 +0000

    Added a button example file

---
+++ functional-react/src/partialApplication.jsx
@@ -0,0 +1,9 @@
+export const Button = ({ size, color, text, ...props }) => {
+	return (
+		<button style={{
+			padding: size === 'large' ? '32px' : '8px',
+			fontSize: size === 'large' ? '32px' : '16px',
+			backgroundColor: color,
+		}} {...props}>{text}</button>
+	);
+}
