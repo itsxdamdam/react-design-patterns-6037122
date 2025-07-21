d5057e9627c4a834ff3f9fe7565c9e1722e0caf3
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2025-07-21 18:41:03 +0100

    complete List and items component

---
+++ layout-component/src/RegularList.jsx
@@ -0,0 +1,9 @@
+const RegularList = ({ items, resourceName, itemComponent: ItemComponent }) => (
+  <>
+    {items.map((item, i) => (
+      <ItemComponent key={i} {...{ [resourceName]: item }} />
+    ))}
+  </>
+);
+
+export default RegularList;
