e24ef4bf4a9a6eed78a969730543e35dfa73ca78
Bakare Samuel Oluwadamilola <49197462+itsxdamdam@users.noreply.github.com>
2025-07-23 11:11:17 +0100

    container component - DataLoader component

--- container-component/src/App.jsx
+++ container-component/src/App.jsx
@@ -1,13 +1,38 @@
+import { DataLoader } from "./DataLoader";
 import { ResourceLoader } from "./ResourceLoader";
 import { UserInfo } from "./UserInfo";
 import { UserLoader } from "./UserLoader";
+import axios from "axios";
+
+const getServerData = (url) => async () => {
+  const response = await axios.get(url);
+  return response.data;
+};
+
+const getTestData = () => {
+  return {
+    id: "1234",
+    name: "Blah Blah",
+    age: 65,
+    hairColor: "gold",
+    hobbies: ["swimming", "running", "games"],
+  };
+};
 
 function App() {
   return (
     <>
+      <DataLoader
+        getDataFn={getTestData}
+        resourceName="user"
+      >
+        <UserInfo />
+      </DataLoader>
+
       <UserLoader userId="234">
         <UserInfo />
       </UserLoader>
+
       <ResourceLoader resourceUrl={`/api/users/345`} resourceName="user">
         <UserInfo />
       </ResourceLoader>
